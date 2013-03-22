d8bf099bd2c5901046fe0b76ae0220e7ace8a4c2
Alexander Naspo <alex.naspo@gmail.com>
2013-03-22 16:08:48 -0400

    removed default lang, updated comments

--- spellchecker.py
+++ spellchecker.py
@@ -2,21 +2,22 @@ import sublime
 import sublime_plugin
 import sys
 import os
+# current solution to include the third party library pyenchant
 sys.path.append(os.path.join(os.path.dirname(__file__), "pyenchant"))
 import enchant
+
+# @TODO multiple selection spell check
 
 
 class SpellCheckerCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         self.edit = edit
-        # for region in self.view.sel():
         self.region = self.view.sel()
-        # @TODO multiple selection spell check
+
         phrase = self.view.substr(self.region[0])
         if not phrase:
             return  # nothing selected
-        # @TODO add language to settings
-        dictionary = enchant.Dict("en_US")
+        dictionary = enchant.Dict()
         self.suggestions = dictionary.suggest(phrase)
         if not dictionary.check(phrase):
             self.view.window().show_quick_panel(self.suggestions, self.replace, sublime.MONOSPACE_FONT)
